1542ecdac1f93858fc1ea0a5ab82dba9aa353608
aliyar <aliar.ulanov@gmail.com>
2021-01-15 11:02:43 +0600

    initial commit

---
+++ git_commands.py
@@ -0,0 +1,28 @@
+# git init - .git - она отвечает за инициализацию нашего проекта при 
+# вызове данной команды создаеться папка .git  
+
+#git remote add name url - она добовляет удаленный репозиторий который
+#			 находиться на удаленном сервере
+
+# git pull origin master - при вызове данной команды мы стягиваем изменения с определенной ветки 
+
+#git status - показывает статус файла изменены,добавлены,ожидающиеся
+
+#git add - она добавляет вайлы в рабосий папки в индех
+
+# git commit m "comment" - она добавляет все файлы в индехсе и во внутренюю базу 
+
+# git branch - вызывает список веток
+
+# git branch name branch
+# git checout name branch
+# git push origin master
+# git reset filename - delete file in staygen area
+
+
+
+
+
+
+
+
